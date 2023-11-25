8e528a66a030fb0efb6e2ba890dc009117c45fb7
cakra budiman <cakrabudiman@icloud.com>
2023-11-25 12:30:56 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "TunnelKit",
     platforms: [
-        .iOS(.v13), .macOS(.v12)
+        .iOS(.v14), .macOS(.v12)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
